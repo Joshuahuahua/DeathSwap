1c46ea666000233789bb379645c7fc56c160bef7
Nelio <46218259+ioNelio@users.noreply.github.com>
2020-07-08 00:29:19 +0800

    Updated to include prefix

--- src/com/github/joshuahuahua/DeathSwap/message.java
+++ src/com/github/joshuahuahua/DeathSwap/message.java
@@ -1,20 +1,22 @@
 package com.github.joshuahuahua.DeathSwap;
 
+import com.github.joshuahuahua.DeathSwap.files.customConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class message {
+
     public static void global(String message) {
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('$',message));
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('$', customConfig.get().getString("prefix") + message));
     }
 
     public static void sender(CommandSender sender, String message) {
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('$',message));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('$',customConfig.get().getString("prefix") + message));
     }
 
     public static void player(Player player, String message) {
-        player.sendMessage(ChatColor.translateAlternateColorCodes('$',message));
+        player.sendMessage(ChatColor.translateAlternateColorCodes('$',customConfig.get().getString("prefix") + message));
     }
 }
