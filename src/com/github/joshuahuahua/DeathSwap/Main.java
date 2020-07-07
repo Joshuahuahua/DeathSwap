237bede9808ad21f4f4c758ce5ff6e4da8b4517a
Joshuahuahua <joshuahollander2001@gmail.com>
2020-07-08 00:04:43 +0800

    Error handled everything

--- src/com/github/joshuahuahua/DeathSwap/Main.java
+++ src/com/github/joshuahuahua/DeathSwap/Main.java
@@ -4,6 +4,7 @@ package com.github.joshuahuahua.DeathSwap;
 import com.github.joshuahuahua.DeathSwap.files.customConfig;
 import com.github.joshuahuahua.DeathSwap.listeners.onDeath;
 import com.github.joshuahuahua.DeathSwap.listeners.onJoin;
+import com.github.joshuahuahua.DeathSwap.listeners.onLeave;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -11,7 +12,6 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -32,7 +32,7 @@ public class Main extends JavaPlugin {
     static int countDown;
     int secondsRemaining;
     public static boolean isRunning;
-    Player host = null;
+    public static Player host = null;
     public static List<Player> lobby = new ArrayList<>();
 
 
@@ -44,6 +44,7 @@ public class Main extends JavaPlugin {
         PluginManager pluginManager  = getServer().getPluginManager();
         pluginManager.registerEvents(new onJoin(this), this);
         pluginManager.registerEvents(new onDeath(this), this);
+        pluginManager.registerEvents(new onLeave(this), this);
 
 
         getLogger().info("DeathSwap Enabled");
@@ -93,7 +94,8 @@ public class Main extends JavaPlugin {
             //################################# /ds reload ################################
             if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
                 customConfig.reload();
-                message.sender(sender, "$a/ds start: $fStarts DeathSwap match");
+                message.sender(sender, "$aDeathSwap reload complete");
+                return true;
             }
 
 
@@ -107,26 +109,32 @@ public class Main extends JavaPlugin {
                 message.sender(sender,"$6/ds leave: $fLeaves current lobby");
                 message.sender(sender,"$6/ds start: $fStarts DeathSwap match");
                 message.sender(sender,"$6/ds stop: $fStops DeathSwap match");
+                return true;
             }
-
 
             //############################## /ds timeset <num> ################################
             if (args.length == 2 && args[0].equalsIgnoreCase("timeset")) {
-                if (sender.getName().equals(host.getName())) {
-                    if (!isRunning) {
-                        time = Integer.parseInt(args[1])*60;
-                        message.sender(sender,"Set time to$e " + time/60);
+                if (host != null) {
+                    if (sender.getName().equals(host.getName())) {
+                        if (!isRunning) {
+                            time = Integer.parseInt(args[1])*60;
+                            message.sender(sender,"Set time to$e " + time/60);
+                        } else {
+                            message.sender(sender,"$cYou can not change the game mid-game!");
+                        }
                     } else {
-                        message.sender(sender,"$cYou can not change the game mid-game!");
+                        sender.sendMessage("$cOnly the host can do that!");
                     }
                 } else {
-                    sender.sendMessage("$4Only the host can do that!");
+                    message.sender(sender,"$cThere is no one hosting DeathSwap!");
+                    message.sender(sender,"$dUse /ds create to host a lobby!");
                 }
+                return true;
             }
 
 
             //############################## /ds create ################################
-            if (args.length == 1 && (args[0].equalsIgnoreCase("create")) || args[0].equalsIgnoreCase("host")) {
+            if (args.length == 1 && (args[0].equalsIgnoreCase("create") || args[0].equalsIgnoreCase("host"))) {
                 if (host == null) {
                     host = (Player) sender;
                     lobby.add(host);
@@ -136,8 +144,8 @@ public class Main extends JavaPlugin {
                     message.sender(sender,"$c$l" + host.getName() + " $r$cis already hosting DeathSwap!");
                     message.sender(sender,"$dUse /ds join to join!");
                 }
+                return true;
             }
-
 
 
             //############################## /ds join ################################
@@ -155,29 +163,36 @@ public class Main extends JavaPlugin {
                     }
                 } else {
                     message.sender(sender,"$cThere are no available games to join!");
-                    message.sender(sender,"$6Use /ds create to create one!");
+                    message.sender(sender,"$dUse /ds create to host a lobby!");
                 }
+                return true;
             }
 
 
 
             //############################## /ds leave ################################
             if (args.length == 1 && args[0].equalsIgnoreCase("leave")) {
-                if (lobby.contains((Player) sender)) {
-                    lobby.remove((Player) sender);
-                    message.global("$6" + sender.getName() + " $chas left the lobby!");
-                } else {
-                    message.sender(sender,"You are not in a lobby!");
-                    message.sender(sender,"$6Use /ds create to create one or /ds join to join an existing one!");
-                }
-                if (sender.getName().equals(host.getName())) {
-                    message.global("$4" + host.getName() + " $chas closed the lobby!");
-                    for (Player player : lobby) {
-                        message.player(player,"$cYou have been removed from the lobby!");
+                if (host != null) {
+                    if (lobby.contains((Player) sender)) {
+                        lobby.remove((Player) sender);
+                        message.global("$6" + sender.getName() + " $chas left the lobby!");
+                    } else {
+                        message.sender(sender,"You are not in a lobby!");
+                        message.sender(sender,"$6Use /ds create to create one or /ds join to join an existing one!");
                     }
-                    host = null;
-                    lobby.clear();
+                    if (sender.getName().equals(host.getName())) {
+                        message.global("$4" + host.getName() + " $chas closed the lobby!");
+                        for (Player player : lobby) {
+                            message.player(player,"$cYou have been removed from the lobby!");
+                        }
+                        host = null;
+                        lobby.clear();
+                    }
+                } else {
+                    message.sender(sender,"$cThere is no one hosting DeathSwap!");
+                    message.sender(sender,"$dUse /ds create to host a lobby!");
                 }
+                return true;
             }
 
 
@@ -185,97 +200,111 @@ public class Main extends JavaPlugin {
             //############################## /ds stop ################################
 
             if (args.length == 1 && args[0].equalsIgnoreCase("stop")) {
-                if (sender.getName().equals(host.getName())) {
-                    if (isRunning) {
-                        isRunning = false;
-                        stopSchedulers();
-                        message.global("$c$lDeath Swap Stopped");
+                if (host != null) {
+                    if (sender.getName().equals(host.getName())) {
+                        if (isRunning) {
+                            isRunning = false;
+                            stopSchedulers();
+                            message.global("$c$lDeath Swap Stopped");
+                        } else {
+                            message.sender(sender,"$cNo active Death Swap!");
+                        }
                     } else {
-                        message.sender(sender,"$cNo active Death Swap!");
+                        message.sender(sender,"$cOnly the host can do that!");
                     }
-                } else {
-                    message.sender(sender,"$cOnly the host can do that!");
                 }
+                return true;
             }
-
 
 
             //############################# START ################################
             if (args.length == 1 && args[0].equalsIgnoreCase("start")) {
-                if (sender.getName().equals(host.getName())) {
-                    if (!isRunning) {
-                        if (lobby.size() > 1) {
-                            isRunning = true;
-                            secondsRemaining = time;
+                if (host != null) {
+                    if (sender.getName().equals(host.getName())) {
+                        if (!isRunning) {
+                            if (lobby.size() > 1) {
+                                isRunning = true;
+                                secondsRemaining = time;
 
-                            World world = Bukkit.getServer().getWorld("world");
-                            assert world != null;
-                            world.setTime(0);
+                                World world = Bukkit.getServer().getWorld("world");
+                                assert world != null;
+                                world.setTime(0);
 
-                            //############################# Init Players ###################
-                            for (Player player : lobby) {
-                                player.setGameMode(GameMode.SURVIVAL);
-                                player.getInventory().clear();
-                                player.setHealth(20);
-                                player.setFoodLevel(20);
+                                //############################# Init Players ###################
+                                for (Player player : lobby) {
+                                    player.setGameMode(GameMode.SURVIVAL);
+                                    player.getInventory().clear();
+                                    player.setHealth(20);
+                                    player.setFoodLevel(20);
 
-                                double playerX = Math.random() * ( 1000 - -1000 );
-                                double playerY = Math.random() * ( 1000 - -1000 );
-                                Location startCoord = new Location(world,playerX,200,playerY);
-                                player.teleport(startCoord);
-                                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE,8*20,200));
+                                    double playerX = Math.random() * ( 1000 - -1000 );
+                                    double playerY = Math.random() * ( 1000 - -1000 );
+                                    Location startCoord = new Location(world,playerX,200,playerY);
+                                    player.teleport(startCoord);
+                                    player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE,8*20,200));
+                                }
+
+                                message.global("$c$lDeath Swap Started");
+                                int divTime = time/60;
+                                message.global("$4Swap time minutes:$c$l " + divTime);
+                                BukkitScheduler scheduler = getServer().getScheduler();
+
+
+
+                                //############################# Create ################################
+                                timer = scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        Location player1pos = lobby.get(0).getLocation();
+                                        int i;
+                                        for (i = 0; i < lobby.size(); i++) {
+                                            if (lobby.get(i) == lobby.get(lobby.size() - 1)) {
+                                                lobby.get(i).teleport(player1pos);
+                                            } else {
+                                                lobby.get(i).teleport(lobby.get(i+1).getLocation());
+                                            }
+                                        }
+                                        secondsRemaining = time;
+                                    }
+                                }, 20*time, 20*time);
+
+
+
+                                countDown = scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        secondsRemaining-=1;
+                                        for (int i : new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+                                            if (i == secondsRemaining) {
+                                                int addedSeconds = i+1;
+                                                message.global("$c$l" + addedSeconds + " $r$4Seconds remaining!");
+                                            }
+                                        }
+                                    }
+                                }, 0, 20);
+                            } else {
+                                message.sender(sender,"$cAt least 2 players are required for DeathSwap!");
                             }
-
-                            message.global("$c$lDeath Swap Started");
-                            message.global("$4Swap time minutes:$c$l " + time);
-                            BukkitScheduler scheduler = getServer().getScheduler();
-
-
-
-                            //############################# Create ################################
-                            timer = scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    Location player1pos = lobby.get(0).getLocation();
-                                    int i;
-                                    for (i = 0; i < lobby.size(); i++) {
-                                        if (lobby.get(i) == lobby.get(lobby.size() - 1)) {
-                                            lobby.get(i).teleport(player1pos);
-                                        } else {
-                                            lobby.get(i).teleport(lobby.get(i+1).getLocation());
-                                        }
-                                    }
-                                    secondsRemaining = time;
-                                }
-                            }, 20*time, 20*time);
-
-
-
-                            countDown = scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-                                    secondsRemaining-=1;
-                                    for (int i : new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
-                                        if (i == secondsRemaining) {
-                                            message.global("$c$l" + i+1 + " $r$4Seconds remaining!");
-                                        }
-                                    }
-                                }
-                            }, 0, 20);
                         } else {
-                            message.sender(sender,"$cAt least 2 players are required for DeathSwap!");
+                            message.sender(sender,"$cGame already in progress!");
                         }
                     } else {
-                        message.sender(sender,"$cGame already in progress!");
+                        message.sender(sender,"$cOnly the host can do that!");
                     }
                 } else {
-                    message.sender(sender,"$cOnly the host can do that!");
+                    message.sender(sender,"$cThere is no one hosting DeathSwap!");
+                    message.sender(sender,"$dUse /ds create to host a lobby!");
                 }
+                return true;
             }
+            message.sender(sender,"$cInvalid command!");
+            message.sender(sender,"$cUse /ds help for a list of available commands.");
             return true;
+        } else {
+            message.global("return false");
+            return false;
         }
-        return false;
     }
 
     // Stops the timers. Used in /ds stop and in the onDeath listener. Not really needed but neatens up code a bit.
